6bc403aa18fa020f2a685c10d249cfc4881c0746
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-12-16 08:27:46 -0300

    add: new project to utils folder

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,9 +1,10 @@
 export const projects = [
   {id: 1, name: 'Gofinance', platform: 'back-end API written in Go', url: 'https://github.com/KaioMarxDEV/gofinance'},
-  {id: 2, name: 'GofinanceWeb', platform: 'front-end consuming gofinance API', url: 'https://github.com/KaioMarxDEV/'},
-  {id: 3, name: 'Podcaster', platform: 'Website with podcast player', url: 'https://github.com/KaioMarxDEV/'},
-  {id: 4, name: 'Coffee', platform: 'Website to delivering coffee', url: 'https://github.com/KaioMarxDEV/'},
-  {id: 5, name: 'ignews', platform: 'Website of news', url: 'https://github.com/KaioMarxDEV/'},
-  {id: 6, name: 'timer', platform: 'Pomodoro style website', url: 'https://github.com/KaioMarxDEV/'},
-  {id: 7, name: 'todo', platform: 'Productivity Website to control time', url: 'https://github.com/KaioMarxDEV/'},
+  {id: 2, name: 'GofinanceWeb', platform: 'front-end consuming gofinance API', url: 'https://github.com/KaioMarxDEV/gofinanceWeb'},
+  {id: 3, name: 'Ecommerce', platform: 'Website of ecommerce', url: 'https://github.com/KaioMarxDEV/'},
+  {id: 4, name: 'Podcaster', platform: 'Website with podcast player', url: 'https://github.com/KaioMarxDEV/'},
+  {id: 5, name: 'Coffee', platform: 'Website to delivering coffee', url: 'https://github.com/KaioMarxDEV/'},
+  {id: 6, name: 'ignews', platform: 'Website of news', url: 'https://github.com/KaioMarxDEV/'},
+  {id: 7, name: 'timer', platform: 'Pomodoro style website', url: 'https://github.com/KaioMarxDEV/timer'},
+  {id: 8, name: 'todo', platform: 'Productivity Website to control time', url: 'https://github.com/KaioMarxDEV/toDO-Website'},
 ]
