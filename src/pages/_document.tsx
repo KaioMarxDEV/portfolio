0569425e694fcb4ab44e7d6cb677d5cdb0ca9f08
KaioMarxDEV <kaiomarxdev@gmail.com>
2023-01-01 15:37:03 -0300

    fix: remove clarity add: new favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,14 +11,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="icon" href='/favicon.ico' />
         <link rel="manifest" href="/site.webmanifest" />
-        <script type="text/javascript">
-          (function(c,l,a,r,i,t,y){
-            // @ts-ignore
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "f7m1bltuuh");
-        </script>
+        
       </Head>
       <body>
         <Main />
